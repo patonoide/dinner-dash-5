6d6944c5f4f31780f7849b7ad975175e40fdfe68
Gabriel Porto Oliveira <gabrielpoliveira@ig.com.br>
2018-07-06 16:42:22 -0300

    Finished category delete

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -7,13 +7,25 @@ class CategoryController < ApplicationController
 
     end
 
-    def update
+    def edit
         @category = Category.find(params[:id])
-        @category.update(category_params)
+        if @category.update(category_params)
+            redirect_to categories_path
+        end
     end
 
     def new
-        @category = Category.new
+        @category = Category.new(category_params)
+        if @category.save
+            redirect_to categories_path
+        end
+    end
+
+    def destroy
+        @category = Category.find(params[:id])
+        if @category.delete
+            redirect_to categories_path
+        end
     end
 
 end
