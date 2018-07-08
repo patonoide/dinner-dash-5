5195120d0a1e7193901bad6e223a1ffc13910177
Gabriel Porto Oliveira <gabrielpoliveira@ig.com.br>
2018-07-08 01:23:21 -0300

    Finished Category CRUD

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,8 @@
-class CategoryController < ApplicationController
-    before_action :require_admin
+class CategoriesController < ApplicationController
 
+    def category_params
+        params.require(:category).permit(:name)
+    end
 
     def index
         @categories = Category.all
@@ -9,21 +11,30 @@ class CategoryController < ApplicationController
 
     def edit
         @category = Category.find(params[:id])
+    end
+
+    def update
+        @category = Category.find(params[:id])
         if @category.update(category_params)
             redirect_to categories_path
         end
     end
 
     def new
-        @category = Category.new(category_params)
-        if @category.save
-            redirect_to categories_path
-        end
+        @category = Category.new
+
     end
 
     def destroy
         @category = Category.find(params[:id])
         if @category.delete
+            redirect_to categories_path
+        end
+    end
+
+    def create
+        @category = Category.new(category_params)
+        if @category.save
             redirect_to categories_path
         end
     end
