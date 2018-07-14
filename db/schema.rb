a6e179e9d83b432adbdf07f86d816b3bd7add1d4
Gabriel Porto Oliveira <gabrielpoliveira@ig.com.br>
2018-07-14 11:55:01 -0300

    Fixed log in screen path

--- db/schema.rb
+++ db/schema.rb
@@ -65,10 +65,8 @@ ActiveRecord::Schema.define(version: 2018_07_09_015208) do
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
-
     t.string "name", null: false
     t.integer "admin", default: 0
-
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
