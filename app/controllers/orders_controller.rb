cc1b6bf3f700b5c7435f0196a3817b3468fd4e84
Vitor Ribas <vitorbandeira@cjr.org.br>
2018-07-16 12:53:19 -0300

    fixed user_id in orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -70,6 +70,6 @@ class OrdersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
-      params.require(:order).permit()
+      params.require(:order).permit(:user_id)
     end
 end
